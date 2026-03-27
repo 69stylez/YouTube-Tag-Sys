1a1209819cc8aa6c2b5ca66590d42dab56f6e9fd
Stefan Seidel <stesei@MacBookPro.home>
2026-03-27 16:40:43 +0100

    Update deployed OAuth and sheet config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = 'DEIN_GOOGLE_CLIENT_ID.apps.googleusercontent.com';
-const SHEET_ID = 'DEINE_GOOGLE_SHEET_ID';
+const CLIENT_ID = '811591415329-13tjb1mumo0hub5nj6sjq4kiuimd7o5t.apps.googleusercontent.com';
+const SHEET_ID = '1mpFm--Sr67iKR3-OqO_C9IzlKaPpeowwx_rRoll1qi4';
 const OAUTH_SCOPES = [
   'https://www.googleapis.com/auth/spreadsheets',
   'openid',
