c99c529e4d81ef549b8ab1324de2e3b9aa401da9
Stefan Seidel <stesei@MacBookPro.home>
2026-03-27 19:03:49 +0100

    Set token proxy URL to deployed Cloudflare Worker

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = '811591415329-13tjb1mumo0hub5nj6sjq4kiuimd7o5t.apps.googleusercontent.com';
 const SHEET_ID = '1mpFm--Sr67iKR3-OqO_C9IzlKaPpeowwx_rRoll1qi4';
-const TOKEN_PROXY_URL = 'https://YOUR-WORKER-SUBDOMAIN.workers.dev/oauth/token';
+const TOKEN_PROXY_URL = 'https://youtube-tag-token-proxy.69stylez.workers.dev';
 const OAUTH_SCOPES = [
   'https://www.googleapis.com/auth/spreadsheets',
   'openid',
